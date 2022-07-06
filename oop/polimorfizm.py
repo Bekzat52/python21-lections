8843511e8a7571b507e96731b1a9f946f4d1ca38
Bekzat52 <bekzat@bekzat-IdeaPad-3-15ALC6>
2022-07-06 10:22:45 +0600

    poli

---
+++ oop/polimorfizm.py
@@ -0,0 +1,25 @@
+"=============== Полиморфизм =============="
+# Полиморфизм - принцип ООП, в котором методы в разных классах называются одинаково.(Один интерфейс - разный функционал)
+
+class Dog:
+    def speak(self):
+        print('гав-гав')
+        
+class Cat:
+    def speak(self):
+        print("мяу-мяу")
+
+class Frog:
+    def speak(self):
+        print('ква-ква')
+
+animals= [Cat(),Dog(),Cat(),Frog(),Frog()]
+
+for animal in animals:
+    animal.speak()  #мяу-мяу
+                    #гав-гав
+                    #мяу-мяу
+                    #ква-ква
+                    #ква-ква
+
+
