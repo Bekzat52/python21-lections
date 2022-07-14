d5db600c9a55531b9bd7f3f8fd754f44e66e45bc
Bekzat52 <bekzat@bekzat-IdeaPad-3-15ALC6>
2022-07-14 12:56:14 +0600

    razbor

--- basics/razbor.py
+++ basics/razbor.py
@@ -10,3 +10,77 @@ class Cat:
         print("Мяу")
 kity = Cat()
 kity.voice() #  Мяу
+
+# from abc import ABC, abstractmethod, abstractproperty
+
+# class AbstractAnimal(ABC):
+#     has_heart = True
+
+#     @abstractmethod
+#     def live(self):
+#         pass
+
+
+# class Dog(AbstractAnimal):
+#     def live(self):
+#         print('hrllo')
+
+# dog = Dog()
+# print(dog.live)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class RadioMixin:
+#     def play(self):
+#         print('Playing music')
+
+
+# class Car(list, RadioMixin):
+#     pass
+
+# class Boat:
+#     pass
+
+
+
+
+
+
+
+
+
+
+# class Person:
+#     name = 'Nastya'
+
+#     @staticmethod
+#     def create_person():
+#         pass
+
+# person = Person()
+# print(person.create_person())
+
+
+
+
+
+
+
+
+# import json
+
+
+# python_obj = [{'name': 'Nastya'}, 1, 2, 3]
+
+# print(json.dumps(python_obj))
